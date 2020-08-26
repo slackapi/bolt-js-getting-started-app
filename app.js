756700887f7a05c9e51de16ebb4e0b286cccb2dc
Michael Brooks <mbrooks@slack-corp.com>
2020-08-26 16:01:44 -0700

    Add event listener for 'hello'

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const app = new App({
   signingSecret: process.env.SLACK_APP_BOLT_JS_GETTING_STARTED_SIGNING_SECRET
 });
 
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
