8fb83cf4ceac95bc458effa88ff709e009910d06
Sarah Jiang <sarahjiang@slack-corp.com>
2021-07-15 14:34:28 +1000

    Update

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const { App } = require('@slack/bolt');
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN
 });
 
 // Listens to incoming messages that contain "hello"
